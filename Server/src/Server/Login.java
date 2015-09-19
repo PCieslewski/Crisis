b258a4983daceafd60a3e4fd3461fe2b346d364b
Will Livesey <Steelerfan2010@ufl.edu>
2015-09-19 15:06:17 -0400

    Test

---
+++ Server/src/Server/Login.java
@@ -0,0 +1,53 @@
+package Server;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String fname = request.getParameter("fname").toString();
+		String lname = request.getParameter("lname").toString();
+		
+		PrintWriter pr = response.getWriter();
+		pr.println("First Name : " + fname);
+		pr.println("Last Name : " + lname);
+		pr.println("Test Complete.\n");
+		pr.println("SERVER WARNING : Will sucks at fantasy football.");
+		
+		//doGet(request, response);
+	}
+
+}
