1690a6e9a33dffb0412fdb765556757ad5fb829e
Will Livesey <Steelerfan2010@ufl.edu>
2015-10-02 19:20:32 -0400

    refactored stuff

--- Backend/Scrape/src/com/backend/hibernate/Main.java
+++ Backend/Scrape/src/com/backend/hibernate/Main.java
@@ -3,8 +3,6 @@ package com.backend.hibernate;
 import java.util.Scanner;
 
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -17,14 +15,15 @@ public class Main {
 	private static final String WEBPAGE_URL = "https://www.isis.ufl.edu/cgi-bin/nirvana?MDASTRAN=RSI-FSCHED";
 
 	public static void main(String args[]) {
-
-		System.out.println("Lol will...");
 		
-		Schedule empty = new Schedule();
-		Person will = new Person("LIVESEY WILLIAM T", "06/27/1993",
-				"COMPUTER ENGINEERING", "ENGINEERING", 21);
+		//want to return person object
+		Parse.parseSchedule(getWebpageSourceCode(enterCredentials()));
 		
-		persistPerson(will);
+//		Schedule empty = new Schedule();
+//		Person will = new Person("LIVESEY WILLIAM T", "06/27/1993",
+//				"COMPUTER ENGINEERING", "ENGINEERING", 21);
+//		
+//		persistPerson(will);
 	}
 
 	private static void persistPerson(Person bob) {
@@ -34,7 +33,7 @@ public class Main {
 		session.getTransaction().commit();
 	}
 	
-	private static void scrapeIsis() {
+	private static LoginCredentials enterCredentials() {
 		Scanner input = new Scanner(System.in);
 		LoginCredentials login = new LoginCredentials();
 
@@ -43,67 +42,28 @@ public class Main {
 
 		System.out.print("Enter your password: ");
 		login.setPassword(input.nextLine());
-
-		String parse = getWebpageSourceCode(login);
-
-		int tableStart = getIndexOfTargetString(parse, "<table", 1);
-		int tableEnd = getIndexOfTargetString(parse, "</table", 2);
-
-		String smallPartOfFile = parse.substring(tableStart - 1, tableEnd - 1);
-		System.out.println(smallPartOfFile);
-	}
-
-	private static Person ngsMethod(String smallPartOfFile) {
-		Person ng38 = new Person();
-
-		// parse input parameter into below stuff
-		// you should probably use getIndexOfTargetString
-
-		ng38.setName("");
-		ng38.setBirthday("");
-		ng38.setMajor("");
-		ng38.setCollege("");
-
-		return ng38;
-	}
-
-	private static int getIndexOfTargetString(String wholeFile, String goal,
-			int numberOfOccurance) {
-		int fromIndex = 0;
-
-		while (numberOfOccurance > 0) {
-			fromIndex = (wholeFile.indexOf(goal, fromIndex)) + 1;
-
-			if (fromIndex == 0) {
-				return -1;
-			}
-			numberOfOccurance--;
-		}
-		return fromIndex;
+		
+		return login;
 	}
 
 	private static String getWebpageSourceCode(LoginCredentials login) {
+		String ufTitle = "UF Authentication - GatorLink Login » University of Florida"; 
 		WebDriver driver = new FirefoxDriver();
 
 		driver.get(WEBPAGE_URL);
 
-		while (!driver.getTitle().equals(
-				"UF Authentication - GatorLink Login » University of Florida"))
-			;
+		while (!driver.getTitle().equals(ufTitle));
 
 		WebElement username = driver.findElement(By.name("j_username"));
 		username.sendKeys(login.getUsername());
-		// username.sendKeys(user);
 
 		WebElement password = driver.findElement(By.name("j_password"));
 		password.sendKeys(login.getPassword());
-		// password.sendKeys(passwd);
 
 		WebElement submit_button = driver.findElement(By.name("login"));
 		submit_button.submit();
 
-		while (!driver.getTitle().equals("ISIS"))
-			;
+		while (!driver.getTitle().equals("ISIS"));
 
 		String webpageUrl = driver.getPageSource();
 		driver.quit();
