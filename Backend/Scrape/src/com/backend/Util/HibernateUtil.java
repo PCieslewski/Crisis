917beff717b7570f1bf8af3df054b8819a51429d
Will Livesey <Steelerfan2010@ufl.edu>
2015-10-02 19:00:53 -0400

    Switched to hibernate 4

--- Backend/Scrape/src/com/backend/Util/HibernateUtil.java
+++ Backend/Scrape/src/com/backend/Util/HibernateUtil.java
@@ -3,17 +3,17 @@ package com.backend.Util;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+@SuppressWarnings("deprecation")
 public class HibernateUtil {
 
-   private static SessionFactory sessionFactory;
-
+  private static SessionFactory sessionFactory;
+   
    static {
 	   try {
 		   sessionFactory = new Configuration().configure().buildSessionFactory();
 	   }
 	   catch (Throwable ex) {
 		   System.err.println("Session factory wasn't created " + ex);
-		   //throw error 
 	   }
    }
    
